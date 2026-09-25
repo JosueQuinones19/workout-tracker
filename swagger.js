ce6b374f35983312db73ba9ae2f75e2f0a0ba65c
JosueQuinones98 <josue.quinones.sig@gmail.com>
2026-09-24 23:56:44 -0600

    Fix Swagger host for Render deployment

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Workout Tracker API',
     description: 'API documentation for the Workout Tracker project'
   },
-  host: 'localhost:8080',
-  schemes: ['http']
+  host: 'workout-tracker-tnwm.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
